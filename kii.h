efbfdb78eb7c4a0fec4e7bdff9239dd293ef3a46
satoshi <satoshi.kumano@kii.com>
2015-02-25 15:31:25 +0900

    Fixed kii struct.

--- kii.h
+++ kii.h
@@ -53,7 +53,8 @@ typedef struct kii_t
     char* app_id;
     char* app_key;
     char* app_host;
-    char buff[4096];
+    char* buff;
+    size_t buff_size;
 
     void* http_context;
     KII_HTTPCB_SET_REQUEST_LINE_PTR http_set_request_line_cb;
