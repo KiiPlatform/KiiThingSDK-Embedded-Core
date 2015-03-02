3e277ccea221443c8cfb37cbfddfb429897c663e
Riza <riza.alaudin@kii.com>
2015-03-02 19:01:31 +0900

    Add CC3200 example

---
+++ CC3200/kii.c
@@ -0,0 +1,232 @@
+#include "kii.h"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+    kii_state_t
+kii_get_state(kii_t* kii)
+{
+    return kii->_state;
+}
+
+    kii_error_code_t
+kii_run(kii_t* kii)
+{
+    kii_http_client_code_t cbr;
+    switch(kii->_state) {
+        case KII_STATE_IDLE:
+            return KIIE_FAIL;
+        case KII_STATE_READY:
+            kii->_state = KII_STATE_EXECUTE;
+            return KIIE_OK;
+        case KII_STATE_EXECUTE:
+            cbr = kii->http_execute_cb(
+                    kii->http_context,
+                    &(kii->response_code),
+                    &(kii->response_body));
+            if (cbr == KII_HTTPC_OK) {
+                kii->_state = KII_STATE_IDLE;
+                return KIIE_OK;
+            } else if (cbr == KII_HTTPC_AGAIN) {
+                return KIIE_OK;
+            } else {
+                kii->_state = KII_STATE_IDLE;
+                return KIIE_FAIL;
+            }
+        default:
+            return KIIE_FAIL;
+    }
+}
+
+    static void
+prv_content_length_str(
+        size_t content_length,
+        char* buff,
+        size_t buff_len)
+{
+    snprintf(buff, buff_len, "%d", content_length);
+}
+
+    static void
+prv_set_thing_register_path(kii_t* kii)
+{
+    sprintf(kii->_http_request_path,
+            "api/apps/%s/things",
+            kii->app_id);
+}
+
+    kii_error_code_t
+kii_register_thing(
+        kii_t* kii,
+        const char* thing_data)
+{
+    prv_set_thing_register_path(kii);
+    kii->http_set_request_line_cb(
+            kii->http_context,
+            "POST",
+            kii->app_host,
+            kii->_http_request_path);
+    kii->http_set_header_cb(
+            kii->http_context,
+            "Connection",
+            "Close"
+            );        
+    kii->http_set_header_cb(
+            kii->http_context,
+            "content-type",
+            "application/vnd.kii.ThingRegistrationAndAuthorizationRequest+json"
+            );
+    kii->http_set_header_cb(
+            kii->http_context,
+            "x-kii-appid",
+            kii->app_id);
+    kii->http_set_header_cb(
+            kii->http_context,
+            "x-kii-appkey",
+            kii->app_key);
+    char content_length[8];
+    memset(content_length, 0x00, 8);
+    prv_content_length_str(strlen(thing_data), content_length, 8);
+    kii->http_set_header_cb(
+            kii->http_context,
+            "content-length",
+            content_length);
+    kii->http_set_body_cb(
+            kii->http_context,
+            thing_data);
+
+    kii->_state = KII_STATE_READY;
+    return KIIE_OK;
+}
+
+    kii_error_code_t
+kii_create_new_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_data)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_create_new_object_with_id(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* object_data)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_patch_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* patch_data,
+        const char* opt_etag)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_replace_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* replace_data,
+        const char* opt_etag)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_get_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_delete_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_subscribe_bucket(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_unsubscribe_bucket(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_subscribe_topic(
+        kii_t* kii,
+        const char* access_token,
+        const kii_topic_t* topic)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_unsubscribe_topic(
+        kii_t* app,
+        const char* access_token,
+        const kii_topic_t* topic)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_install_thing_push(
+        kii_t* kii,
+        const char* access_token,
+        kii_bool_t development)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
+    kii_error_code_t
+kii_get_mqtt_endpoint(
+        kii_t* kii,
+        const char* access_token,
+        const char** installation_id)
+{
+    /* TODO: implement. */
+    return KIIE_FAIL;
+}
+
---
+++ CC3200/kii.h
@@ -0,0 +1,180 @@
+#ifndef _kii_
+#define _kii_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+
+typedef enum kii_bool_t {
+    KII_FALSE = 0,
+    KII_TRUE
+} kii_bool_t;
+
+typedef enum kii_http_client_code_t {
+    KII_HTTPC_OK = 0,
+    KII_HTTPC_FAIL,
+    KII_HTTPC_AGAIN
+} kii_http_client_code_t;
+
+typedef kii_http_client_code_t
+        (*KII_HTTPCB_SET_REQUEST_LINE_PTR)(
+                void* http_context,
+                const char* method,
+                const char* host,
+                const char* path);
+typedef kii_http_client_code_t
+        (*KII_HTTPCB_SET_HEADER_PTR)(
+                void* http_context,
+                const char* key,
+                const char* value);
+typedef kii_http_client_code_t
+        (*KII_HTTPCB_SET_BODY_PTR)(
+                void* http_context,
+                const char* body_data);
+typedef kii_http_client_code_t
+        (*KII_HTTPCB_EXECUTE)(
+                void* http_context,
+                int* response_code,
+                char** response_body);
+
+typedef enum kii_error_code_t {
+    KIIE_OK = 0,
+    KIIE_FAIL
+} kii_error_code_t;
+
+typedef enum kii_state_t {
+    KII_STATE_IDLE = 0,
+    KII_STATE_READY,
+    KII_STATE_EXECUTE
+} kii_state_t;
+
+typedef struct kii_t
+{
+    char* app_id;
+    char* app_key;
+    char* app_host;
+    char* buffer;
+    size_t buffer_size;
+    int response_code;
+    char* response_body;
+
+    void* http_context;
+    KII_HTTPCB_SET_REQUEST_LINE_PTR http_set_request_line_cb;
+    KII_HTTPCB_SET_HEADER_PTR http_set_header_cb;
+    KII_HTTPCB_SET_BODY_PTR http_set_body_cb;
+    KII_HTTPCB_EXECUTE http_execute_cb;
+    char _http_request_path[256];
+
+    kii_state_t _state;
+} kii_t;
+
+typedef enum kii_scope_type_t {
+    KII_SCOPE_APP,
+    KII_SCOPE_USER,
+    KII_SCOPE_GROUP,
+    KII_SCOPE_THING
+} kii_scope_type_t;
+
+typedef struct kii_bucket_t {
+    kii_scope_type_t scope;
+    char* scope_id;
+    char* bucket_name;
+} kii_bucket_t;
+
+typedef struct kii_topic_t {
+    kii_scope_type_t scope;
+    char* scope_id;
+    char* topic_name;
+} kii_topic_t;
+
+kii_state_t kii_get_state(kii_t* kii);
+kii_error_code_t kii_run(kii_t* kii);
+
+kii_error_code_t
+kii_register_thing(kii_t* kii,
+        const char* thing_data);
+
+kii_error_code_t
+kii_create_new_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_data);
+
+kii_error_code_t
+kii_create_new_object_with_id(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* object_data);
+
+kii_error_code_t
+kii_patch_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* patch_data,
+        const char* opt_etag);
+
+kii_error_code_t
+kii_replace_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id,
+        const char* replace_data,
+        const char* opt_etag);
+
+kii_error_code_t
+kii_get_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id);
+
+kii_error_code_t
+kii_delete_object(
+        kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket,
+        const char* object_id);
+
+kii_error_code_t
+kii_subscribe_bucket(kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket);
+
+kii_error_code_t
+kii_unsubscribe_bucket(kii_t* kii,
+        const char* access_token,
+        const kii_bucket_t* bucket);
+
+kii_error_code_t
+kii_subscribe_topic(kii_t* kii,
+        const char* access_token,
+        const kii_topic_t* topic);
+
+kii_error_code_t
+kii_unsubscribe_topic(kii_t* app,
+        const char* access_token,
+        const kii_topic_t* topic);
+
+kii_error_code_t
+kii_install_thing_push(kii_t* kii,
+        const char* access_token,
+        kii_bool_t development);
+
+kii_error_code_t
+kii_get_mqtt_endpoint(kii_t* kii,
+        const char* access_token,
+        const char** installation_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
