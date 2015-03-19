81d2200b1165b3a919a2c9cc5e8cea80f0ffe436
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-03-19 14:31:07 +0900

    Settings for demo application are moved to kii_cloud_demo_setting.h

--- gt202/kii_cloud_demo.c
+++ gt202/kii_cloud_demo.c
@@ -2,15 +2,6 @@
 
 #include "gt202_kii_adapter.h"
 
-#define APP_ID "84fff36e"
-#define APP_KEY "e45fcc2d31d6aca675af639bc5f04a26"
-#define APP_HOST "api-development-jp.internal.kii.com"
-#define THING_ID "th.53ae324be5a0-26f8-4e11-a13c-03da6fb2"
-#define ACCESS_TOKEN "ablTGrnsE20rSRBFKPnJkWyTaeqQ50msqUizvR_61hU"
-#define BUCKET_NAME "myBucket"
-#define TOPIC_NAME "myTopic"
-#define OBJECT_NAME "my_object"
-
 void parse_response(char* resp_body)
 {
     /* TODO: implement */
