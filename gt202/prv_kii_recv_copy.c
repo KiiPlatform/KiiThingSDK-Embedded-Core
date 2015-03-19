a8b8f40fdde7a50ee63b502379c13fef5a170d9c
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-03-19 14:28:38 +0900

    File names are changed.

--- gt202/prv_kii_recv_copy.c
+++ gt202/prv_kii_recv_copy.c
@@ -1,6 +1,6 @@
 #include "gt202_kii_adapter.h"
 
-#include "prv_kii_ssl_setting.h"
+#include "prv_kii_setting.h"
 
 #if ZERO_COPY
 #error
--- gt202/prv_kii_ssl_setting.h
+++
@@ -1,6 +0,0 @@
-#ifndef _prv_kii_ssl_setting
-#define _prv_kii_ssl_setting
-
-#define CONNECT_SSL 0
-
-#endif
